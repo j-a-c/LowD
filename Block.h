95e80dabc34aca641d6da71edc3d109435bd1b11
j-a-c <runguy@ymail.com>
2013-12-18 13:44:37 -0500

    Block class cleanup

--- Block.h
+++ Block.h
@@ -35,12 +35,18 @@ class Block
     private:
         // BlockType default is Air
         BlockType _blockType;
+
+        // Half the length, width, height
+        static constexpr float length = BLOCK_LENGTH / 2.0f;
+        static constexpr float height = BLOCK_HEIGHT / 2.0f;
+        static constexpr float width = BLOCK_WIDTH / 2.0f;
 };
 
 Block::Block()
 {
     // Each block is by default Air until the ChunkManager changes its
     _blockType = BlockType_Air;
+
 }
 
 Block::~Block()
@@ -48,83 +54,8 @@ Block::~Block()
 
 }
 
-/**
- * Creates an instance of a single cube.
- * Centered around (0,0,0)
- */
-void Block::createBlock()
-{
-    // Half the length, width, height
-    float length = BLOCK_LENGTH / 2.0f;
-    float height = BLOCK_HEIGHT / 2.0f;
-    float width = BLOCK_WIDTH / 2.0f;
-    
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    glBegin(GL_QUADS);
-    
-    glColor3f(0.0, 1.0, 0.0);
-
-
-    // Back
-    glNormal3f(0.0f, 0.0f, -1.0f);
-    glVertex3f(0.0f+width, 0.0f-height, 0.0f-length); // BR
-    glVertex3f(0.0f+width, 0.0f+height, 0.0f-length); //TR
-    glVertex3f(0.0f-width, 0.0f+height, 0.0f-length); // TL
-    glVertex3f(0.0f-width, 0.0f-height, 0.0f-length); // BL
-
-    // Front
-    glNormal3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(0.0f+width, 0.0f-height, 0.0f+length); //BR
-    glVertex3f(0.0f+width, 0.0f+height, 0.0f+length); //TR
-    glVertex3f(0.0f-width, 0.0f+height, 0.0f+length); // TL
-    glVertex3f(0.0f-width, 0.0f-height, 0.0f+length); // BL
-
-    // Right
-    glNormal3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f+width, 0.0f-height, 0.0f-length); // BB
-    glVertex3f(0.0f+width, 0.0f-height, 0.0f+length); // BT
-    glVertex3f(0.0f+width, 0.0f+height, 0.0f+length); // FT
-    glVertex3f(0.0f+width, 0.0f+height, 0.0f-length); // FB
-
-    // Left
-    glNormal3f(-1.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f-width, 0.0f-height, 0.0f+length); //FB
-    glVertex3f(0.0f-width, 0.0f+height, 0.0f+length); //FT
-    glVertex3f(0.0f-width, 0.0f+height, 0.0f-length); //BT
-    glVertex3f(0.0f-width, 0.0f-height, 0.0f-length); //BB
-
-    // Bottom
-    glNormal3f(0.0f, -1.0f, 0.0f);
-    glVertex3f(0.0f-width, 0.0f-height, 0.0f-length); // BL
-    glVertex3f(0.0f+width, 0.0f-height, 0.0f-length); // BR
-    glVertex3f(0.0f+width, 0.0f-height, 0.0f+length); // FR
-    glVertex3f(0.0f-width, 0.0f-height, 0.0f+length); // FL
-
-    // Top
-    glNormal3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(0.0f-width, 0.0f+height, 0.0f+length); // FL
-    glVertex3f(0.0f+width, 0.0f+height, 0.0f+length); // FR
-    glVertex3f(0.0f+width, 0.0f+height, 0.0f-length); //BR
-    glVertex3f(0.0f-width, 0.0f+height, 0.0f-length); // BL
-
-    glEnd();
-
-}
-
 void Block::createFront()
 {
-    // Half the length, width, height
-    float length = BLOCK_LENGTH / 2.0f;
-    float height = BLOCK_HEIGHT / 2.0f;
-    float width = BLOCK_WIDTH / 2.0f;
-    
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    //glBegin(GL_QUADS);
-    
     glColor3f(0.0, 1.0, 0.0);
 
     // Front
@@ -136,23 +67,10 @@ void Block::createFront()
     glVertex3f(0.0f-width, 0.0f+height, 0.0f+length); // TL
     glVertex3f(0.0f-width, 0.0f-height, 0.0f+length); // BL
     glVertex3f(0.0f+width, 0.0f+height, 0.0f+length); //TR
-
-
-    //glEnd();
 }
 
 void Block::createBack()
 {
-    // Half the length, width, height
-    float length = BLOCK_LENGTH / 2.0f;
-    float height = BLOCK_HEIGHT / 2.0f;
-    float width = BLOCK_WIDTH / 2.0f;
-    
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    //glBegin(GL_QUADS);
-    
     glColor3f(0.0, 1.0, 0.0);
 
     // Back
@@ -164,22 +82,10 @@ void Block::createBack()
     glVertex3f(0.0f-width, 0.0f+height, 0.0f-length); // TL
     glVertex3f(0.0f-width, 0.0f-height, 0.0f-length); // BL
     glVertex3f(0.0f+width, 0.0f+height, 0.0f-length); //TR
-
-    //glEnd();
 }
 
 void Block::createLeft()
 {
-    // Half the length, width, height
-    float length = BLOCK_LENGTH / 2.0f;
-    float height = BLOCK_HEIGHT / 2.0f;
-    float width = BLOCK_WIDTH / 2.0f;
-    
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    //glBegin(GL_QUADS);
-    
     glColor3f(0.0, 1.0, 0.0);
 
     // Left
@@ -191,22 +97,10 @@ void Block::createLeft()
     glVertex3f(0.0f-width, 0.0f+height, 0.0f-length); //BT
     glVertex3f(0.0f-width, 0.0f-height, 0.0f-length); //BB
     glVertex3f(0.0f-width, 0.0f+height, 0.0f+length); //FT
-
-    //glEnd();
 }
 
 void Block::createRight()
 {
-    // Half the length, width, height
-    float length = BLOCK_LENGTH / 2.0f;
-    float height = BLOCK_HEIGHT / 2.0f;
-    float width = BLOCK_WIDTH / 2.0f;
-    
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    //glBegin(GL_QUADS);
-    
     glColor3f(0.0, 1.0, 0.0);
 
     // Right
@@ -218,22 +112,10 @@ void Block::createRight()
     glVertex3f(0.0f+width, 0.0f+height, 0.0f+length); // FT
     glVertex3f(0.0f+width, 0.0f+height, 0.0f-length); // FB
     glVertex3f(0.0f+width, 0.0f-height, 0.0f-length); // BB
-
-    //glEnd();
 }
 
 void Block::createTop()
 {
-    // Half the length, width, height
-    float length = BLOCK_LENGTH / 2.0f;
-    float height = BLOCK_HEIGHT / 2.0f;
-    float width = BLOCK_WIDTH / 2.0f;
-    
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    //glBegin(GL_QUADS);
-    
     glColor3f(0.0, 1.0, 0.0);
 
     // Top
@@ -245,22 +127,10 @@ void Block::createTop()
     glVertex3f(0.0f+width, 0.0f+height, 0.0f-length); //BR
     glVertex3f(0.0f-width, 0.0f+height, 0.0f-length); // BL
     glVertex3f(0.0f-width, 0.0f+height, 0.0f+length); // FL
-
-    //glEnd();
 }
 
 void Block::createBottom()
 {
-    // Half the length, width, height
-    float length = BLOCK_LENGTH / 2.0f;
-    float height = BLOCK_HEIGHT / 2.0f;
-    float width = BLOCK_WIDTH / 2.0f;
-    
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    //glBegin(GL_QUADS);
-    
     glColor3f(0.0, 1.0, 0.0);
 
     // Bottom
@@ -273,8 +143,6 @@ void Block::createBottom()
     glVertex3f(0.0f+width, 0.0f-height, 0.0f+length); // FR
     glVertex3f(0.0f-width, 0.0f-height, 0.0f+length); // FL
     glVertex3f(0.0f+width, 0.0f-height, 0.0f-length); // BR
-
-    //glEnd();
 }
 
 void Block::setType(BlockType blockType)
